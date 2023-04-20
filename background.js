5ef695573a197f7bc25ea8d38569cdea4de18542
geni94 <eyzeiri@ritech.co>
2023-04-20 23:28:16 +0200

    enhance + comment

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-// chrome.action.onClicked.addListener((tab) => {
-//   chrome.tabs.sendMessage(tab.id, { action: 'applyHighlighting' });
-// });
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.tabs.sendMessage(tab.id, { action: 'toggleHighlighting' });
--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,23 @@
-let wordsHighlighted = false;
+let isHighlightingActive = false;
 
+// Applies highlighting to the first three letters of each word in the input text.
 function highlightFirstThreeLetters(text) {
   return text.replace(/(\b\w{1,3})(\w*)/g, '<span class="adhd-container"><strong class="adhd-highlight">$1</strong>$2</span>');
 }
 
+// Processes the DOM nodes recursively based on the specified action (apply or remove highlighting).
 function processNode(node, action) {
-  if (node.nodeType === Node.TEXT_NODE && node.parentElement.tagName.toLowerCase() !== 'script') {
+  if (node.nodeType === Node.TEXT_NODE && !node.parentElement.closest('script, style')) {
+    const parent = node.parentElement;
     if (action === 'apply') {
       const newNode = document.createElement('span');
       newNode.classList.add('adhd-container');
       newNode.innerHTML = highlightFirstThreeLetters(node.textContent);
-      node.parentElement.replaceChild(newNode, node);
-    } else if (action === 'remove') {
-      const parent = node.parentElement;
+      parent.replaceChild(newNode, node);
+    } else if (action === 'remove' && parent.classList.contains('adhd-container')) {
       parent.replaceChild(document.createTextNode(node.textContent), node);
-      if (parent.classList.contains('adhd-container')) {
-        const grandParent = parent.parentElement;
-        grandParent.replaceChild(document.createTextNode(parent.textContent), parent);
-      }
+      const grandParent = parent.parentElement;
+      grandParent.replaceChild(document.createTextNode(parent.textContent), parent);
     }
   } else if (node.nodeType === Node.ELEMENT_NODE) {
     for (let i = 0; i < node.childNodes.length; i++) {
@@ -26,11 +26,12 @@ function processNode(node, action) {
   }
 }
 
+// Applies highlighting to the first three letters of each word on the page.
 function applyHighlighting() {
-  const body = document.body;
-  processNode(body, 'apply');
+  processNode(document.body, 'apply');
 }
 
+// Removes highlighting from the first three letters of each word on the page.
 function clearHighlighting() {
   const highlightedElements = document.querySelectorAll('.adhd-highlight');
   const highlightedContainers = document.querySelectorAll('.adhd-container');
@@ -44,13 +45,14 @@ function clearHighlighting() {
   });
 }
 
+// Listens for messages from the background script to toggle highlighting.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'toggleHighlighting') {
-    if (wordsHighlighted) {
+    if (isHighlightingActive) {
       clearHighlighting();
     } else {
       applyHighlighting();
     }
-    wordsHighlighted = !wordsHighlighted;
+    isHighlightingActive = !isHighlightingActive;
   }
 });
